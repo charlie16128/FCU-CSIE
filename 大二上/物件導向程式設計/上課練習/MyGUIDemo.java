5eaa97d7c86cc946fd69832f36de711655d76632
charlie16128 <ctugm24@gmail.com>
2025-12-04 01:37:00 +0800

    MyGUIDemo add custom Font

--- 大二上/物件導向程式設計/上課練習/MyGUIDemo.java
+++ 大二上/物件導向程式設計/上課練習/MyGUIDemo.java
@@ -1,6 +1,6 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
 
 public class MyGUIDemo {
     
@@ -8,17 +8,20 @@ public class MyGUIDemo {
         JFrame frame = new JFrame("Test");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400,400);
-
+        
         Container cp = frame.getContentPane();
-        cp.setLayout(new GridLayout(3,3));
+        cp.setLayout(new GridLayout(4,3));
 
         ButtonClickHandler myHandler = new ButtonClickHandler();
         for(int i = 1; i <= 9; i++){
-            JButton btn = new JButton("按鈕" + i);
+            JButton btn = new JButton("" + i);
             btn.addActionListener(myHandler);
             cp.add(btn);
+            Font btnFont = new Font("Serif", Font.BOLD, 24);
+            btn.setFont(btnFont);
         }
         frame.setVisible(true);
+        
     }
 
     public static void main(String[] args) {
