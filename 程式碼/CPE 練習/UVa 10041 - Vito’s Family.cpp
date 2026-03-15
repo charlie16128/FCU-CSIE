8368c0794a10b0ee48dfc4930e256af239f27ee4
charlie16128 <ctugm24@gmail.com>
2026-03-15 18:23:47 +0800

    unfinish

---
+++ 程式碼/CPE 練習/UVa 10041 - Vito’s Family.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int tc;
+    cin >> tc;
+
+    while(tc--){
+        int r;
+        vector<int> s(r);
+
+        cin >> r;
+        for(auto &x : s){
+            cin >> x;
+        }
+
+        sort(s.begin(), s.end());
+
+        int mid = s[r / 2];
+
+        
+    }
+
+    return 0;
+}
